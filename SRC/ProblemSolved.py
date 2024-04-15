605db2b8e79e5275fdf5a427e7b33dbc2654a9b9
44462576+PedroLantyer@users.noreply.github.com <pedrolantyer@hotmail.com>
2024-04-15 13:36:07 -0300

    Update ProblemSolved.py

--- SRC/ProblemSolved.py
+++ SRC/ProblemSolved.py
@@ -1,6 +1,19 @@
 import pulp
 import sys
 
+class TextHandling:
+    def __init__(self) -> None:
+        pass
+
+    def ReplaceUnderlines(self, str):
+        if(str.count("_") > 0):
+            formatedStr = ""
+            for char in str:
+                if char == "_": formatedStr += " "
+                else: formatedStr += char
+            return formatedStr
+        return str
+
 class Product:
     def __init__(self, productName, profit, category, lowBound=None, upBound=None) -> None:
         self.productName = productName
@@ -12,7 +25,7 @@ class Product:
         if(upBound != None): self.upBound = upBound
         else: self.upBound = None
 
-    def SetProductVariable(self):
+    def SetVariable(self):
         try:
             self.productVariable = pulp.LpVariable(name=self.productName, lowBound=self.lowBound, upBound=self.upBound, cat=self.category)
         
@@ -24,22 +37,55 @@ class Product:
         else:
             return True
         
-    def GetProductVariable(self):
+    def GetVariable(self):
             return self.productVariable
+
+class ProductionCosts:
+    def __init__(self, varName, category ,lowBound ,upBound=None) -> None:
+        self.varName = varName
+        self.varCategory = category
+        
+        if(lowBound != None): self.lowBound = lowBound
+        else: self.lowBound = None
+        if(upBound != None): self.upBound = upBound
+        else: self.upBound = None
+    
+    def SetVariable(self):
+        try:
+            self.productionCostVariable = pulp.LpVariable(name=self.varName, lowBound=self.lowBound, upBound=self.upBound, cat=self.varCategory)
+
+        except Exception as err:
+            print("Failed to create Variable")
+            print(err)
+            return False
+        
+        else:
+            return True
+
+    def GetVariable(self):
+        return self.productionCostVariable
 
 class Problem:
     def __init__(self, lpSense, problemName = None) -> None:
         self.lpSense = lpSense #-1 FOR MAX / 1 FOR MIN / 0 FOR EQUAL TO
-        if (problemName == None): self.problem = pulp.LpProblem(sense=lpSense)
-        else: self.problem = pulp.LpProblem(name=problemName, sense=lpSense)
+        if (problemName == None): 
+            self.problem = pulp.LpProblem(sense=lpSense)
+            self.problemName = None
+        else: 
+            self.problem = pulp.LpProblem(name=problemName, sense=lpSense)
+            self.problemName = problemName
+        self.InitalizeClasses()
 
-    def SetVariableList(self, productList):
+    def InitalizeClasses(self):
+        self.txtHandling = TextHandling()
+
+    def SetVariableList(self, varList):
         try:
             self.varList = []
             
-            for i in range(len(productList)):
-                if not(productList[i].SetProductVariable()): raise Exception("Failed to get Variables")
-                self.varList.append(productList[i].GetProductVariable())
+            for i in range(len(varList)):
+                if not(varList[i].SetVariable()): raise Exception("Failed to get Variables")
+                self.varList.append(varList[i].GetVariable())
         
         except Exception as err:
             print(err)
@@ -47,13 +93,21 @@ class Problem:
 
         else:
             return True
-        
-    def SetNumModifiers(self, productList):
+    
+    def SetNumModifiers(self, varList):
         try:
             self.numModifierList = []
-            for i in range(len(productList)):
-                num = int(productList[i].productProfitMargin)
-                self.numModifierList.append(num)
+            for i in range(len(varList)):
+                
+                match (self.problemName):
+                    case "Problema Primal":
+                        numMod = int(varList[i].productProfitMargin)
+                    case "Problema Dual":
+                        numMod = int(varList[i].upBound)
+                    case _:
+                        raise Exception("Invalid Problem type")
+                
+                self.numModifierList.append(numMod)
         
         except Exception as err:
             print("Failed to set num modifiers")
@@ -100,6 +154,16 @@ class Problem:
         else:
             return True
 
+    def SetOptimalValue(self, optimalValue):
+        try:
+            self.optimalValue = optimalValue
+        except Exception as err:
+            print(err)
+            print(f"Failed to set optimalValue for {self.problemName}")
+
+    def GetOptimalValue(self):
+        return self.optimalValue
+
     def PrintPrimalProblemResults(self):
         try:
             profit = 0
@@ -111,10 +175,12 @@ class Problem:
                 varNames.append(self.varList[i].name)
                 profit += (varValues[i] * self.numModifierList[i])
 
+
+            self.SetOptimalValue(profit)
             print("Resultados:")
             print("Lucro: R$%.2f" % profit)
             for i in range(len(varValues)):
-                print("%s: %d unidade(s)" % (varNames[i], varValues[i]))
+                print("%s: %d unidade(s)" % (self.txtHandling.ReplaceUnderlines(varNames[i]), varValues[i]))
             print("")
         
         except Exception as err:
@@ -152,11 +218,38 @@ def PrimalProblem():
         if not(primalProblem.SolveProblem()): raise Exception("Failed to solve problem")
 
         if (primalProblem.status == 1):
-            if(primalProblem.PrintPrimalProblemResults()): raise Exception("Failed to print results")
+            if not(primalProblem.PrintPrimalProblemResults()): raise Exception("Failed to print results")
 
     except Exception as err:
         print(err)
         print("Failed to solve primal problem")
+
+def DualProblem():
+    prodCostsCafeDaManha = ProductionCosts(varName="Custo de produção do cafe da manha", category="Integer", lowBound=10)
+    prodCostsAlmoco = ProductionCosts(varName="Custo de produção do almoço", category="Integer", lowBound=15)
+    prodCostsCoffeeBreak = ProductionCosts(varName="Custo de produção do coffee break", category="Integer", lowBound=8)
+    prodCostsJantar = ProductionCosts(varName="Custo de produção do jantar", category="Integer", lowBound=22)
+
+    dualProblem = Problem(lpSense=1, problemName="Problema Dual")
+    prodCosts = [prodCostsCafeDaManha, prodCostsAlmoco, prodCostsCoffeeBreak, prodCostsJantar]
+
+    try:
+        if not(dualProblem.SetVariableList(prodCosts)): raise Exception("Failed to set variable list")
+        if not(dualProblem.SetNumModifiers(prodCosts)): raise Exception("Failed to set profit margins")
+        if not(dualProblem.SetObjective()): raise Exception("Failed to set objective")
+
+
+    pass
+    """Objetivo: Min Σ(Yi*Bi)
+        Bi = {130; 70; 150; 55}
+        Y = {Conjunto com os custos de produção}
+        (4*Y1)+(3*Y2) > 60
+        Y4 - Y2 >= 7
+        Y1 >= 10
+        Y2 >= 15
+        Y3 >= 8
+        Y4 >= 22
+        Σ(Yi) >= 55"""
 
 if __name__ == "__main__":
 
